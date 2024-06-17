7f4882485fd4e6cab26c6267c1143a745e7333ac
eiderpeña <eapenad@unal.edu.co>
2024-06-16 22:22:25 -0500

    cliente vs1.1

--- Clientes/App_cliente.py
+++ Clientes/App_cliente.py
@@ -73,4 +73,4 @@ def status():
     return jsonify({"send_count": send_count, "data": current_data})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
